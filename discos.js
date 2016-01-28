49311938fc565f1335193f56861a6d699c65dd73
FERNANDOGC20 <fernando_gonzalez_cruz@hotmail.com>
2016-01-28 14:26:57 -0600

    inicio

---
+++ discos.js
@@ -0,0 +1,17 @@
+$(document).on('ready',inicia);
+//$(function(){
+//});
+function inicia()
+{
+	$('button').on('click',compra);
+}
+function compra()
+{
+var nodo=$(this).parent().children().first().next().text();
+console.log(nodo);
+$('aside').append('<li>'+nodo+'<li>');
+var precio=$(this).parent().children().first().next().next().next().text();
+
+precio=parseInt(precio);
+
+}
